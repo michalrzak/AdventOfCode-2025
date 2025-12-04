54dc610cbedfa2ea9f0a2d0e7d6667d547d55cce
michalrzak <michal.r.zak@pm.me>
2025-12-04 07:00:20 +0100

    added day 4

---
+++ src/aoc_2025/day_04_02.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+def main():
+    with open("data/day_04.txt", "r") as file:
+        raw = file.read()
+
+    raw = raw.strip()
+    lines = raw.split("\n")
+
+    grid = defaultdict(lambda: '.')
+    paper_positions = []
+    for y, row in enumerate(lines):
+        for x, c in enumerate(row):
+            grid[(x, y)] = c
+            if c == '@':
+                paper_positions.append((x, y))
+
+    neighbours = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    accessible = 0
+    removed_something = True
+    while removed_something:
+
+        removed_something = False
+        new_paper_positions = []
+        for pos in paper_positions:
+            neighbour_count = 0
+            for n in neighbours:
+                new_pos = (pos[0] + n[0], pos[1] + n[1])
+                if grid[new_pos] == '@':
+                    neighbour_count += 1
+            if neighbour_count < 4:
+                accessible += 1
+                grid[pos] = '.'
+                removed_something = True
+            else:
+                new_paper_positions.append(pos)
+
+        paper_positions = new_paper_positions
+    print(accessible)
+
+
+if __name__ == "__main__":
+    main()
+
